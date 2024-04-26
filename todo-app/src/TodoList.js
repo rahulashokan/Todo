9ccb5a42295d4084c33c36eeb123c80de7a8659a
Rahul <raht009@gmail.com>
2024-04-26 19:27:22 +0530

    done the movement

--- todo-app/src/TodoList.js
+++ todo-app/src/TodoList.js
@@ -2,32 +2,36 @@ import React, { useState } from "react";
 import "./style.css";
 
 function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const [progress, setProgress] = useState([]);
-  const [done, setDone] = useState([]);
+  const [todoList, setTodoList] = useState([]);
+  const [inProgressList, setInProgressList] = useState([]);
+  const [doneList, setDoneList] = useState([]);
 
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState([]);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
-  const handleTodo = (e) => {
+  const handleAddTodo = (category, index) => {
     if (inputValue.trim() !== "") {
-      setTodos([...todos, inputValue]);
-      setInputValue("");
-    }
-  };
-
-  const handleProgress = (e) => {
-    if (inputValue.trim() !== "") {
-      setProgress([...progress, inputValue]);
-      setInputValue("");
-    }
-  };
-  const handleDone = (e) => {
-    if (inputValue.trim() !== "") {
-      setDone([...done, inputValue]);
-      setInputValue("");
+      switch (category) {
+        case "todo":
+          setTodoList([...todoList, inputValue]);
+          break;
+        case "inProgress":
+          setInProgressList([...inProgressList, todoList[index]]);
+          const newTodoList = [...todoList];
+          newTodoList.splice(index, 1);
+          setTodoList(newTodoList);
+          break;
+        case "done":
+          setDoneList([...doneList, inProgressList[index]]);
+          const newInProgressList = [...inProgressList];
+          newInProgressList.splice(index, 1);
+          setInProgressList(newInProgressList);
+          break;
+        default:
+          break;
+      }
     }
   };
 
@@ -41,9 +45,9 @@ function TodoList() {
           value={inputValue}
           onChange={handleInputChange}
         />
-        <button onClick={() => handleTodo()}>Add Todo</button>
-        <button onClick={() => handleProgress()}>In progress</button>
-        <button onClick={() => handleDone()}>Done</button>
+        <button onClick={() => handleAddTodo("todo")}>Add Todo</button>
+        {/* <button onClick={() => handleProgress()}>In progress</button>
+        <button onClick={() => handleDone()}>Done</button> */}
       </div>
       <div className="d-flex flex-row">
         <div class="card">
@@ -51,8 +55,15 @@ function TodoList() {
             <h2>Todo</h2>
             <div calss="card-body"></div>
             <ul>
-              {todos.map((todo, index) => (
-                <li key={index}>{todo}</li>
+              {todoList.map((todo, index) => (
+                <li key={index}>
+                  {todo}
+                  {
+                    <button onClick={() => handleAddTodo("inProgress", index)}>
+                      Add In Progress
+                    </button>
+                  }
+                </li>
               ))}
             </ul>
           </div>
@@ -63,8 +74,15 @@ function TodoList() {
             <h2>Progress</h2>
             <div calss="card-body"></div>
             <ul>
-              {progress.map((todo, index) => (
-                <li key={index}>{todo}</li>
+              {inProgressList.map((todo, index) => (
+                <li key={index} draggable>
+                  {todo}
+                  {
+                    <button onClick={() => handleAddTodo("done", index)}>
+                      Add Done
+                    </button>
+                  }
+                </li>
               ))}
             </ul>
           </div>
@@ -74,7 +92,7 @@ function TodoList() {
             <h2>Done</h2>
             <div calss="card-body"></div>
             <ul>
-              {done.map((todo, index) => (
+              {doneList.map((todo, index) => (
                 <li key={index}>{todo}</li>
               ))}
             </ul>
